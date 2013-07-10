e3e47e8c08100ff1115e9f3e67309580b463a26f
Max L <maxlips@yandex.ru>
2013-07-10 20:59:39 +0000

    dsf

--- web.js
+++ web.js
@@ -3,8 +3,22 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var data = fs.readFile('index.html', 'utf-8');
-  response.write('1 - '+data);
+  var content;
+// First I want to read the file
+fs.readFile('./index.html', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    content = data;
+
+    // Invoke the next step here however you like
+    console.log(content);   // Put all of the code here (not the best solution)
+    processFile();          // Or put the next step in a function and invoke it
+});
+
+function processFile() {
+    console.log(content);
+}
 });
 
 
