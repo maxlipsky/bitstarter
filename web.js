119557463a061695480bf178018b71615afe90e6
Max L <maxlips@yandex.ru>
2013-07-10 21:03:37 +0000

    dsf

--- web.js
+++ web.js
@@ -3,24 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var content;
-// First I want to read the file
-fs.readFile('./index.html', function read(err, data) {
-    if (err) {
-        throw err;
-    }
-    content = data;
-
-    // Invoke the next step here however you like
-    console.log(content);   // Put all of the code here (not the best solution)
-    processFile();          // Or put the next step in a function and invoke it
+  response.write('Ho!');
 });
 
-function processFile() {
-    console.log(content);
-}
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
-
 
 
 var port = process.env.PORT || 5000;
