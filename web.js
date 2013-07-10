d5f952b069625fe47e27c16758ff6a658294de1b
Max L <maxlips@yandex.ru>
2013-07-10 20:27:46 +0000

    web.js

--- web.js
+++ web.js
@@ -6,6 +6,12 @@ app.get('/', function(request, response) {
   response.send('Hello World 3!');
 });
 
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+  response.send(data);
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
