e66655c62a908c66f7747cffd86de16148a8d0bd
Max L <maxlips@yandex.ru>
2013-07-10 21:50:59 +0000

    w

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   
   fs.readFile('./index.html', function (err, data) {
     if (err) throw err;
-    response.send(data);
+    response.write(data);
   });
 });
 
