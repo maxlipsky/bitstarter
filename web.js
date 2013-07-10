7a025c20279f0b94b0a17249220431ea7c633cfd
Max L <maxlips@yandex.ru>
2013-07-10 21:07:48 +0000

    dsf

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.write('Ho!');
+  response.send('Ho!');
 });
 
 
